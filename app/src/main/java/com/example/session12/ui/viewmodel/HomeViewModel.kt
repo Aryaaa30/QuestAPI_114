79507028fa16f7dfe9079c13887ffd4c57bf419b
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-29 19:56:33 +0700

    Menambahkan HomeViewModel, Membuat sealed class HomeUiState dan fungsi getMhs()

---
+++ app/src/main/java/com/example/session12/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.session12.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.session12.model.Mahasiswa
+import com.example.session12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class HomeUiState {
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+}
