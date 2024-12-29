1161f3b39d5a6ab3a2ff4d6bce0564b541a121da
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-29 18:33:58 +0700

    Manambahkan dan menyelesaikan code di bagian MahasiswaService

---
+++ app/src/main/java/com/example/session12/dependenciesinjection/MahasiswaContainer.kt
@@ -0,0 +1,27 @@
+package com.example.session12.dependenciesinjection
+
+import com.example.session12.repository.MahasiswaRepository
+import com.example.session12.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer {
+    private val baseUrl = "http://10.0.0.2:8000/umyTI/" //localhost diganti ip kalo run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkKontakRepository(mahasiswaService) }
+}
--- app/src/main/java/com/example/session12/service/MahasiswaService.kt
+++ app/src/main/java/com/example/session12/service/MahasiswaService.kt
@@ -1,28 +1,32 @@
 package com.example.session12.service
 
 import com.example.session12.model.Mahasiswa
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Query
 
-interface MahasiswaService{
+interface MahasiswaService {
     @Headers(
-        "Accept: application/json",
-        "Content_Type: applicaton/json",
-    )
-
+    "Accept: application/json",
+    "Content-Type: application/json"
+)
     @GET("bacamahasiswa.php")
     suspend fun getAllMahasiswa(): List<Mahasiswa>
 
-    @GET("baca1mahasiswa.php/{nim}")
-    suspend fun getMahasiswabyNim(@Query("nim") nim : String): Mahasiswa
+    @GET("bacama1hasiswa.php/{nim")
+    suspend fun getMahasiswabyNim(@Query("nim") nim:String):Mahasiswa
 
     @POST("insertmahasiswa.php")
-    suspend fun inserMahasiswa(@Body mahasiswa: Mahasiswa)
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
 
     @PUT("editmahasiswa/{nim}")
-    suspend fun
+    suspend fun updateMahasiswa(@Query("nim")nim:String, @Body mahasiswa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php")
+    suspend fun deleteMahasiswa(@Query("nim") nim: String): Response<Void>
 }
