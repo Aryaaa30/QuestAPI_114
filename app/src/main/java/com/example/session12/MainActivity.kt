fe452843c8c63b68e34cca4b8752d1a444d09aa0
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-29 21:44:08 +0700

    Mengubah pemanggilan di MainActivity menjadi ke MahasiswaApp

--- app/src/main/java/com/example/session12/MainActivity.kt
+++ app/src/main/java/com/example/session12/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.session12.ui.MahasiswaApp
 import com.example.session12.ui.theme.Session12Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Session12Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MahasiswaApp()
                 }
             }
         }
