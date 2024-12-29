02e56792314c0122a48ca2b05d229c3f464fc40a
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-29 20:24:55 +0700

    Menambahkan InsertView dan navigasi untuk Entry Mahasiswa

---
+++ app/src/main/java/com/example/session12/ui/view/InsertView.kt
@@ -0,0 +1,24 @@
+package com.example.session12.ui.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.session12.ui.navigation.DestinasiNavigasi
+import com.example.session12.ui.viewmodel.InsertViewModel
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+}
